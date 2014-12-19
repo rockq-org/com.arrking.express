abb90a14aa662ade561f7b62dd841a1160079bd6
Samurais <hailiang.hl.wang@gmail.com>
2014-12-19 11:25:35 +0800

    #17 change app name

--- hooks/after_prepare/copy-jpush-files.js
+++ hooks/after_prepare/copy-jpush-files.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
+
 var path = require("path");
-    fs = require ('fs');
-    ncp = require('ncp').ncp;
+fs = require('fs');
+ncp = require('ncp').ncp;
 
 ncp.limit = 16;
 
 var _NMR = process.env.NODE_PATH;
 if (!_NMR) {
-  console.log("To copy icons during prepare, need to set the NODE_PATH environment variable to point to the system's node_modules path");
-  return;
+    console.log("To copy icons during prepare, need to set the NODE_PATH environment variable to point to the system's node_modules path");
+    return;
 }
 
 var cordova_util = require('cordova-lib/src/cordova/util');
@@ -19,12 +20,12 @@ var projConfig = new ConfigParser(projXml);
 var projName = projConfig.name();
 
 var fileset = {
-  android: {
-            "AndroidManifest.xml": "AndroidManifest.xml",
-			"JPushPlugin.java":  "src/cn/jpush/phonegap/JPushPlugin.java",
-            //"JPushPlugin.js": "assets/www/plugins/cn.jpush.phonegap.JPushPlugin/www/JPushPlugin.js",
-			"Tory.java":  "src/com/arrking/tory/Tory.java"
-	}
+    android: {
+        "AndroidManifest.xml": "AndroidManifest.xml",
+        "JPushPlugin.java": "src/cn/jpush/phonegap/JPushPlugin.java",
+        //"JPushPlugin.js": "assets/www/plugins/cn.jpush.phonegap.JPushPlugin/www/JPushPlugin.js",
+        "Tory.java": "src/com/arrking/tory/Tory.java"
+    }
 };
 
 for (var platform in fileset) {
@@ -39,15 +40,13 @@ for (var platform in fileset) {
         var dest = path.join(dstPath, val);
 
         fs.exists(dest, function(exists) {
-            if (exists) {
-                ncp(src, dest, function (err) {
-                    if (err) {
-                        return console.error(err);
-                    } else {
-                        return console.log("Copied " + src + " to " + dest);
-                    }
-                });
-            }
+            ncp(src, dest, function(err) {
+                if (err) {
+                    return console.error(err);
+                } else {
+                    return console.log("Copied " + src + " to " + dest);
+                }
+            });
         });
     });
 };
