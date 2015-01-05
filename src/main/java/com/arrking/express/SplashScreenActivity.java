086f325a54206ca9b3d13a6179aeed7b8bb50210
Samurais <hailiang.hl.wang@gmail.com>
2015-01-05 21:12:33 +0800

    #24 add splash screen with Handler().postDelayed

---
+++ src/main/java/com/arrking/express/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.arrking.express;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+/**
+ * Created by hain on 05/01/2015.
+ */
+public class SplashScreenActivity extends Activity{
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 10000;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.splash_screen);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreenActivity.this,MainActivity.class);
+                SplashScreenActivity.this.startActivity(mainIntent);
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
