38c2d94a7bf4b07fa0842632f113eabd18eec372
Samurais <hailiang.hl.wang@gmail.com>
2015-01-06 10:09:40 +0800

    #15 add login page

---
+++ src/main/java/com/arrking/express/LoginPageActivity.java
@@ -0,0 +1,16 @@
+package com.arrking.express;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by hain on 06/01/2015.
+ */
+public class LoginPageActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
+}
--- src/main/java/com/arrking/express/SplashScreenActivity.java
+++ src/main/java/com/arrking/express/SplashScreenActivity.java
@@ -5,31 +5,56 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
+
+import com.arrking.android.database.Properties;
+
+import cn.trinea.android.common.util.StringUtils;
 
 /**
  * Created by hain on 05/01/2015.
  */
-public class SplashScreenActivity extends Activity{
-
-    /** Duration of wait **/
+public class SplashScreenActivity extends Activity {
+    private static final String CLASSNAME = SplashScreenActivity.class.getName();
     private final int SPLASH_DISPLAY_LENGTH = 10000;
+    // status flags
+    private final int LOGIN_NONE = 1;
+    private final int LOGIN_AVIL = 2;
 
-    /** Called when the activity is first created. */
+    private static Properties properties;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case LOGIN_NONE:
+                    Intent LgIntent = new Intent(SplashScreenActivity.this, LoginPageActivity.class);
+                    SplashScreenActivity.this.startActivity(LgIntent);
+                    SplashScreenActivity.this.finish();
+                    break;
+                case LOGIN_AVIL:
+                    Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    SplashScreenActivity.this.startActivity(mainIntent);
+                    SplashScreenActivity.this.finish();
+                    break;
+            }
+        }
+    };
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.splash_screen);
+        properties = Properties.getInstance(this);
 
-        /* New Handler to start the Menu-Activity
-         * and close this Splash-Screen after some seconds.*/
-        new Handler().postDelayed(new Runnable(){
-            @Override
-            public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(SplashScreenActivity.this,MainActivity.class);
-                SplashScreenActivity.this.startActivity(mainIntent);
-                SplashScreenActivity.this.finish();
-            }
-        }, SPLASH_DISPLAY_LENGTH);
+        if (StringUtils.isEmpty(properties.get("username"))) {
+            Log.d(CLASSNAME, "user is available.");
+            handler.sendEmptyMessage(LOGIN_NONE);
+        } else {
+            Log.d(CLASSNAME, "user is not available");
+            handler.sendEmptyMessage(LOGIN_AVIL);
+        }
     }
 }
